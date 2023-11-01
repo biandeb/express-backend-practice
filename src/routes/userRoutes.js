34a9d7e94071b0441a6e0fbc93f6055b0de46e11
Bianca Deblinger <biandeb96@gmail.com>
2023-10-31 23:08:29 -0300

    agrego archivos de routes y controller

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+import {
+  deleteUser,
+  getUsers,
+  postUser,
+  putUser,
+} from '../controllers/userController.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { post_userSchema } from '../helpers/validationSchemas/userSchemas.js';
+
+const router = express.Router();
+
+// GET
+router.get('/', getUsers);
+
+// POST
+router.post(
+  '/',
+  // Utilizamos un callback para mandar el schema y hacerlo genérico a validateBody
+  (req, res, next) => validateBody(req, res, next, post_userSchema),
+  postUser,
+);
+
+// PUT
+router.put('/:id', putUser);
+
+// DELETE
+router.delete('/:id', deleteUser);
+
+export default router;
