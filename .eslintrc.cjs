60510c5279a538d73ee25ccd58345b294d64ec15
Bianca Deblinger <biandeb96@gmail.com>
2023-11-02 21:30:36 -0300

    agrego archivos en carpetas más código de clase de hoy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
-    extends: 'airbnb-base',
-    env: {
-      es6: true,
-      browser: true,
-    },
-    rules: {
-      'import/extensions': 'off',
-      camelcase: 'off',
-      'no-restricted-globals': 'off',
-      'import/prefer-default-export': 'off',
-      'no-underscore-dangle': 'off',
-    },
-    ignorePatterns: ['**/node_modules/**', '**/dist/**'],
-  };
+  extends: 'airbnb-base',
+  env: {
+    es6: true,
+    browser: true,
+  },
+  rules: {
+    'import/extensions': 'off',
+    'space-unary-ops': 'off',
+    camelcase: 'off',
+    'no-restricted-globals': 'off',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+  },
+  ignorePatterns: ['**/node_modules/**', '**/dist/**'],
+};
